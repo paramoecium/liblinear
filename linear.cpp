8c41cbc237e4e4f4e59dae0d4d9473ac862b9ea1
jimmypupu <jimmypupu@gmail.com>
2016-07-26 12:57:37 +0800

    fine

--- linear.cpp
+++ linear.cpp
@@ -3129,7 +3129,6 @@ int save_model(const char *model_file_name, const struct model *model_)
 			int	index = 0;
 			while(p != NULL && p[index].index != -1)
 			{
-				fprintf(stderr, "%d ", index);
 				if(model_->bias >= 0 && p[index].index == n)
 					break;
 				fprintf(fp,"%d:%.8g ",p[index].index,p[index].value);
@@ -3268,8 +3267,9 @@ struct model *load_model(const char *model_file_name)
 		else if(strcmp(cmd,"cSV")==0)
 		{
 			int i = nr_class*(nr_class-1)/2;
-			FSCANF(fp,"%d",&nSV);
-			model_->nSV=nSV;
+			model_->cSV = (int*)malloc(i*sizeof(int));
+			for(int j = 0; j < i; j++)
+				FSCANF(fp,"%d",&model_->cSV[j]);
 		}
 		else if(strcmp(cmd,"m1")==0)
 		{
